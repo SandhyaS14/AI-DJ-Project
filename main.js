9464983567a3f43be94a0a936bc354ddb8820be4
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-11-02 19:26:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeft = 0;
+songStatus = "";
+scoreRight = 0;
+song2Status = "";
 
 function preload() {
     song1 = loadSound("hari_neeve.mp3");
@@ -23,6 +27,29 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 500, 500);
+
+    stroke("#ff0000");
+    fill("#ff0000");
+
+    songStatus = song1.isPlaying();
+    song2Status = song2.isPlaying();
+
+    if(scoreLeft > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if(songStatus == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "Song Playing - Hari Neeve";
+        }
+    }
+    if(scoreRight > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if(song2Status == false) {
+            song2.play();
+            document.getElementById("song").innerHTML = "Song Playing - Kadiri Nrusimhudu";
+        }
+    }
 }
 
 function modelLoaded() {
@@ -38,5 +65,8 @@ function gotResults(results) {
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("Right wrist X: " + rightWristX + "Right wrist Y: " + rightWristY);
+
+        scoreLeft = results[0].pose.keypoints[9].score;
+        scoreRight = results[0].pose.keypoints[10].score;
     }
 }
